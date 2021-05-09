d1cb3efdb80d9a9c25e0e225e82608d4143f6ed7
JosteinE <Josteineri@hotmail.com>
2021-05-09 18:37:16 +0200

    Planet now spawns a cube!

--- Source/OrbitalCombat/TerrainFace.cpp
+++ Source/OrbitalCombat/TerrainFace.cpp
@@ -16,9 +16,9 @@ TerrainFace::TerrainFace(int resolution, FVector localUp, int section)
 	uv0.AddZeroed(resolution * resolution); // = new TArray<FVector2D>[resolution * resolution];
 	int triIndex = 0;
 	int index = 0;
-	float meshStart = -0.5f;
-	float meshEnd = 0.5f;
-	float vertStep = 1.f / (resolution - 1.f);
+	float meshStart = -1.f;
+	float meshEnd = 1.f;
+	float vertStep = (meshEnd - meshStart) / (resolution - 1.f);
 	for (float y = meshStart; y <= meshEnd; y += vertStep)		// We add the step amount here for the y axis
 	{
 		for (float x = meshStart; x <= meshEnd; x += vertStep)	// We add the step amount here for the x axis
@@ -28,20 +28,29 @@ TerrainFace::TerrainFace(int resolution, FVector localUp, int section)
 			normals[index] = localUp;
 
 			// Constructing the triangles, making sure we're not going constructing triangles beyond scope
-			if (x <= meshEnd - vertStep && y <= meshEnd - vertStep)
+			if (x < meshEnd && y < meshEnd && triIndex < triangles.Num())
 			{
 				triangles[triIndex] = index;
-				triangles[triIndex + 1] = index + resolution + 1;
-				triangles[triIndex + 2] = index + resolution;
+				triangles[triIndex + 1] = index + resolution;
+				triangles[triIndex + 2] = index + resolution + 1;
 
 				triangles[triIndex + 3] = index;
-				triangles[triIndex + 4] = index + 1;
-				triangles[triIndex + 5] = index + resolution;
+				triangles[triIndex + 4] = index + resolution + 1;
+				triangles[triIndex + 5] = index + 1;
+				//triangles[triIndex] = index;
+				//triangles[triIndex + 1] = index + resolution + 1;
+				//triangles[triIndex + 2] = index + resolution;
+
+				//triangles[triIndex + 3] = index;
+				//triangles[triIndex + 4] = index + 1;
+				//triangles[triIndex + 5] = index + resolution + 1;
 				triIndex += 6;
 			}
 			index++;
+
 		}
 	}
+	UE_LOG(LogTemp, Warning, TEXT("triIndex: %i"), triIndex);
 }
 
 TerrainFace::~TerrainFace()
